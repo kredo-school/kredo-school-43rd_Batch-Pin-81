e01b40f80ccfce100bc485061f8cb4aaf1e9e720
Rikako <111677661+Rikako@users.noreply.github.com>
2026-05-30 15:14:44 +0900

    frontend/signup created, frontend/login edited

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
