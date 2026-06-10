435a3a9f13efc47bf07f01f9e1a9180834dbd87b
Rikako <111677661+Rikako@users.noreply.github.com>
2026-06-10 21:01:44 +0900

    frontend/customer/restaurant-page

--- app/Http/Controllers/RestaurantSearchController.php
+++ app/Http/Controllers/RestaurantSearchController.php
@@ -15,4 +15,19 @@ class RestaurantSearchController extends Controller
     {
         return view('customers.restaurants.index');
     }
+
+    public function show()
+    {
+        $photos = [
+            asset('https://picsum.photos/400/250?random=6'),
+            asset('https://picsum.photos/400/250?random=10'),
+            asset('https://picsum.photos/400/250?random=9'),
+            asset('https://picsum.photos/400/250?random=8'),
+            asset('https://picsum.photos/400/250?random=7'),
+        ];
+
+        shuffle($photos);
+
+        return view('customers.restaurants.show', compact('photos'));
+    }
 }
