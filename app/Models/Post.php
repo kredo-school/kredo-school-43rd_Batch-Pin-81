c911f573f74f77d1d5376382256581a3f0521851
yeajakim94-debug <yeajs.kim94@gmail.com>
2026-06-27 14:41:50 +0900

    Backend/customer_review WIP: Fixing errors (Temporary save to pull Reservation)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,14 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Comment;
+use App\Models\Star;
 
 class Post extends Model
 {
     use HasFactory, SoftDeletes;
     protected $fillable = ['description', 'image', 'user_id', 'restaurant_id'];
     
+    public function user()
+    {
+        return $this->belongTo(User::class);
+    }
+    public function restaurant()
+    {
+        return $this->belongTo(Restaurant::class);
+    }
     public function comments()
     {
         return $this->hasMany(Comment::class);
+    }
+    public function star()
+    {
+        return $this->hasOne(Star::class);
     }
 }
